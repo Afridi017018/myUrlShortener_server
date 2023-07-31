ccaf545114b0ae1335fb26f06e999c1209bf6f34
Afridi017018 <93644242+Afridi017018@users.noreply.github.com>
2023-07-31 22:19:29 +0600

    render modification

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
 const app = require("./app")
-const mongoose =require("mongoose")
+
 
 const port = process.env.port || 5000;
 
-
-
-const dbConnect = async()=>{
-    mongoose.connect(process.env.URL)
-    .then(() => {
-        console.log("DB Connected!!!")
-        app.listen(port,()=>{
-            console.log(`Server is listening on port ${port}`)
-        })
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-}
-
-dbConnect();
-
-// module.exports = dbConnect;
-
+app.listen(port,()=>{
+    console.log(`Server is listening on port ${port}`)
+})
